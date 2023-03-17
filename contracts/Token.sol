0976acac69dd2f8d6e6614c0e0193816ed40145d
Leonid Medovyy <leonid@storypro.io>
2023-03-16 18:59:28 -0700

    added the ability to halt bank sales

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.9;
 // Import Ownable from the OpenZeppelin Contracts library
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "hardhat/console.sol";
 
-contract Token is ERC20, Ownable {
+contract Token is ERC20, Pausable, Ownable {
     constructor(uint256 initialMint, uint256 initialPrice) ERC20("Token", "TKN") {
         _mint(msg.sender, initialMint * 10 ** decimals());
         setPrice(initialPrice);
@@ -27,13 +28,27 @@ contract Token is ERC20, Ownable {
         return _price;
     }
 
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    receive() external payable{
-        uint256 tokens = _price / msg.value;
+    receive() external payable whenNotPaused {
+        console.log('value', _price / msg.value);
+
+        uint256 tokens = msg.value / _price;
         _mint(msg.sender, tokens);
         emit Received(msg.sender, msg.value);
+    }
+
+    function _requireNotPaused() internal view virtual override{
+        require(!paused(), "The bank is not selling tokens right now. Please buy from your peers.");
     }
 }
