7db7813f8b07a62042f770480653f08af811d77d
Leonid Medovyy <leonid@storypro.io>
2023-03-16 17:01:14 -0700

    updating tests for the token

--- test/Token.ts
+++ test/Token.ts
@@ -14,8 +14,14 @@ describe('Token', function () {
   });
 
   // Test case
+  it('has a default value', async function () {
+    // Test if the returned value is the same one
+    const ONE_ETH = 1000_000_000_000_000_000n
+    expect((await this.token.getPrice())).to.equal(ONE_ETH);
+  });
+
   it('stores a new value', async function () {
-    const new_token_price = 2000000000000
+    const new_token_price = 2000_000_000_000_000_000n
 
     // Store a value
     await this.token.setPrice(new_token_price);
